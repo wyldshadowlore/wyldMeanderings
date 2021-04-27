c6acfdd78b6d2f2afd11a10dfe3e695fae07a357
WyldLore Collab <83235587+wyldshadowlore@users.noreply.github.com>
2021-04-26 20:35:52 -0500

    new1.expanded traits

--- enemy.py
+++ enemy.py
@@ -1,15 +1,18 @@
 import random as r
 
 class monster:
-	def __init__(self, name, hp, attack, defense):
+	def __init__(self, name, hp, attack, defense, gold, experience, agility):
 		self.name = name
 		self.hp = hp
 		self.attack = attack
 		self.defense = defense
+		self.gold = gold
+		self.experience = experience
+		self.agility = agility
 
 enemies = [
-('ogre', (15,20,1), (8,10,1), (8,10,1) ),
-('imp', (2,7,1), (1,2,1), (1,2,1) ),
-('wolf', (5,9,1), (1,2,1), (3,4,1) ),
-('lore spider', (1,25,1), (1,11,2), (1,11,2) ),
+('ogre', (15,20,1), (8,10,1), (8,10,1), 100, 10, 5 ),
+('imp', (2,7,1), (1,2,1), (1,2,1), 5, 1, 11 ),
+('wolf', (5,9,1), (1,2,1), (3,4,1), 0, 22, 45 ),
+('lore spider', (1,25,1), (1,11,2), (1,11,2), 12, 14, 21 ),
 ]
---
+++ hero.py
@@ -0,0 +1,12 @@
+import random as r
+
+class hero:
+	def __init__(self, name, hp, attack, defense, gold, experience, level, agility):
+		self.name = name
+		self.hp = hp
+		self.attack = attack
+		self.defense = defense
+		self.gold = gold
+		self.experience = experience
+		self.level = level
+		self.agility = agility
